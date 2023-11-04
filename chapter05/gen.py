1e879e2cc28db54a3dc7414ff7c982047d7a5881
Max Bernstein <max@bernsteinbear.com>
2023-11-04 15:52:34 -0400

    Specify build.ninja fewer times

--- chapter05/gen.py
+++ chapter05/gen.py
@@ -11,20 +11,17 @@ build_dir = pathlib.Path("build")
 src_dir = pathlib.Path("src/main/java")
 srcs = (path for path in src_dir.rglob("*.java"))
 
-with open("build.ninja", "w+") as f:
-    writer = ninja.Writer(f)
-    writer.variable("src_dir", str(src_dir))
-    writer.variable("build_dir", str(build_dir))
-    writer.variable(
-        "flags",
-        cmd(
-            "--class-path", "$src_dir", "--module-path", "$src_dir", "-d", "$build_dir"
-        ),
-    )
-    writer.rule("regen_ninja", cmd(sys.executable, __file__))
-    writer.build("build.ninja", "regen_ninja", implicit=[__file__])
-    writer.rule("javac", cmd("javac", "$flags", "$in"))
-    for src in srcs:
-        classfile = src.relative_to(src_dir).with_suffix(".class")
-        dst = build_dir.joinpath(classfile)
-        writer.build(str(dst), "javac", str(src))
+writer = ninja.Writer(sys.stdout)
+writer.variable("src_dir", str(src_dir))
+writer.variable("build_dir", str(build_dir))
+writer.variable(
+    "flags",
+    cmd("--class-path", "$src_dir", "--module-path", "$src_dir", "-d", "$build_dir"),
+)
+writer.rule("regen_ninja", cmd(sys.executable, "$in", ">", "$out"))
+writer.build("build.ninja", "regen_ninja", inputs=[__file__])
+writer.rule("javac", cmd("javac", "$flags", "$in"))
+for src in srcs:
+    classfile = src.relative_to(src_dir).with_suffix(".class")
+    dst = build_dir.joinpath(classfile)
+    writer.build(str(dst), "javac", str(src))
