8c6e9af6a710c9cc001cda6194b73f56629101d7
Max Bernstein <max@bernsteinbear.com>
2023-11-04 15:24:02 -0400

    Simplify and use pathlib more

--- chapter05/gen.py
+++ chapter05/gen.py
@@ -1,16 +1,31 @@
 import ninja
 import pathlib
 
-srcs = (path for path in pathlib.Path("src/main").rglob("*.java"))
+
+def cmd(*args):
+    return " ".join(str(arg) for arg in args)
+
+
+build_dir = pathlib.Path("build")
+src_dir = pathlib.Path("src/main/java")
+srcs = (path for path in src_dir.rglob("*.java"))
 
 with open("build.ninja", "w+") as f:
     writer = ninja.Writer(f)
     writer.rule(
         "javac",
-        " ".join(
-            ("javac", "-cp", "src/main/java", "--module-path", "src/main/java", "$in")
+        cmd(
+            "javac",
+            "-cp",
+            src_dir,
+            "--module-path",
+            src_dir,
+            "-d",
+            build_dir,
+            "$in",
         ),
     )
     for src in srcs:
-        dst = str(src).removesuffix(".java") + ".class"
-        writer.build(dst, "javac", str(src))
+        classfile = src.relative_to(src_dir).with_suffix(".class")
+        dst = build_dir.joinpath(classfile)
+        writer.build(str(dst), "javac", str(src))
