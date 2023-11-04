dfefcdde436fb319c4f7131b39d13daeccdf3872
Max Bernstein <max@bernsteinbear.com>
2023-11-04 15:56:50 -0400

    Drop --module-path; not needed

--- chapter05/gen.py
+++ chapter05/gen.py
@@ -14,10 +14,7 @@ srcs = (path for path in src_dir.rglob("*.java"))
 writer = ninja.Writer(sys.stdout)
 writer.variable("src_dir", src_dir)
 writer.variable("build_dir", build_dir)
-writer.variable(
-    "flags",
-    cmd("--class-path", "$src_dir", "--module-path", "$src_dir", "-d", "$build_dir"),
-)
+writer.variable("flags", cmd("--class-path", "$src_dir", "-d", "$build_dir"))
 writer.rule("regen_ninja", cmd(sys.executable, "$in", ">", "$out"))
 writer.build("build.ninja", "regen_ninja", __file__)
 writer.rule("javac", cmd("javac", "$flags", "$in"))
